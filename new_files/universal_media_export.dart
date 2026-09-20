dd16df6d58352aeb031381133edb5bb7f53013ba
y7004634-byte <y7004634@gmail.com>
2026-09-20 18:58:27 +0800

    Import iterable helpers for media export

--- new_files/universal_media_export.dart
+++ new_files/universal_media_export.dart
@@ -10,6 +10,7 @@ import 'package:PiliPlus/utils/pilinara_native_bridge.dart';
 import 'package:PiliPlus/utils/share_utils.dart';
 import 'package:PiliPlus/utils/subtitle_utils.dart';
 import 'package:PiliPlus/utils/video_utils.dart';
+import 'package:collection/collection.dart' show IterableExtension;
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:path/path.dart' as path;
